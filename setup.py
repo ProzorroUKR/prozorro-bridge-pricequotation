540a9369202904f78c5cb1e8da738ebd93ad7b8c
Ivan <baturin.ivan9@gmail.com>
2021-10-26 01:55:23 +0300

    add setup file

---
+++ setup.py
@@ -0,0 +1,24 @@
+import os
+from setuptools import setup, find_packages
+
+requires = []
+version = "0.0.0"
+
+with open(os.path.join(os.path.dirname(__file__), "requirements.txt"), "r") as f:
+    for resource in f.readlines():
+        if not resource.startswith("git+"):
+            requires.append(resource.strip())
+        else:
+            res = resource.strip()
+            egg = res.split("#egg=")[1]
+            requires.append("@".join([egg, res]))
+
+setup(
+    name="prozorro_bridge_pricequotation",
+    version=version,
+    description="",
+    packages=find_packages("src"),
+    package_dir={"": "src"},
+    install_requires=requires,
+    include_package_data=True,
+)
