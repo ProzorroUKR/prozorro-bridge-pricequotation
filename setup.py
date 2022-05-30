80d313d027c90f6e698e85e61d599d19762e56d9
Aleksey Kuznetsov <aleksey.kuznetsov@raccoongang.com>
2022-05-30 18:57:42 +0000

    Release 1.0.14

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import os
 from setuptools import setup, find_packages
 
 requires = []
-version = "1.0.13"
+version = "1.0.14"
 
 with open(os.path.join(os.path.dirname(__file__), "requirements.txt"), "r") as f:
     for resource in f.readlines():
